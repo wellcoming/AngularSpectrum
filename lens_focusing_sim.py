0fbaeecf78e4bfa311faf70a17fae6bbfa6d81ed
Wel Coming <coming2wel@gmail.com>
2025-10-28 18:00:46 +0800

    :alembic: Approximate lens diffraction

--- lens_focusing_sim.py
+++ lens_focusing_sim.py
@@ -11,19 +11,19 @@ from angular_spectrum.field import create_spherical_wave
 plt.rcParams["font.sans-serif"] = ["SimHei"]
 
 
-def lens_focusing():
+def lens_focusing_sim():
     """透镜聚焦演示"""
     lens_r = 2.5e-2
     hole_r = 2.5e-3
-    d = 0.5
-    physical_shape = np.array((d * lens_r, d * lens_r))
+    d = 0.25
+    physical_shape = np.array((d * 2 * lens_r, d * 2 * lens_r))
     shape = np.array((2048, 2048))
     delta = physical_shape / shape
 
     wave_length = 550e-9
     focal_length = 50e-2
     L1 = 100e-2
-    L2 = 100e-2 + 100e-3
+    L2 = 100e-2 + 200e-3
 
     color = True
     phase = False
@@ -32,7 +32,7 @@ def lens_focusing():
     create_spherical_wave(field, wave_length, delta, L1, waist_ratio=0)
     create_lens(field, wave_length, focal_length, delta),
     create_circle(field, physical_shape / 2, hole_r, 0, delta),
-    create_circle(field, physical_shape / 2, lens_r, 0, delta, reverse=True),
+    create_circle(field, physical_shape / 2, lens_r * d, 0, delta, reverse=True),
 
     plane = SpatialSlice.from_field(field, delta, 0, wave_length)
     plt.figure(figsize=figure_shape(shape, 2))
@@ -46,9 +46,9 @@ def lens_focusing():
 
     plt.tight_layout()
 
-    plt.savefig("output/lens_focusing.png")
+    plt.savefig("output/lens_focusing_sim.png")
     plt.show()
 
 
 if __name__ == "__main__":
-    lens_focusing()
+    lens_focusing_sim()
